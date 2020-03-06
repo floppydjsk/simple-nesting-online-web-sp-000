fccb18940c5e1e6c648a1bc799db87530db5b409
Daniel Kim <daniel.kim52@gmail.com>
2020-03-06 15:45:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] = "C"     
 end
 
 def adding_matz
