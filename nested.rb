9ffd18c939d391cfdaf68c5f058b5371b2986272
Daniel Kim <daniel.kim52@gmail.com>
2020-03-06 15:42:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
+programmer_hash[:alan_kay][:known_for]
+programmer_hash[:dennis_ritchie][:languages] = "C"
 
 end
 
